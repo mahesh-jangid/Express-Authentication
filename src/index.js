482022c06e8a416ed984114b42591638fcdbd243
mahesh-jangid <mesh.jangid@gmail.com>
2022-03-19 01:02:08 +0530

    Completed

--- src/index.js
+++ src/index.js
@@ -21,10 +21,3 @@ app.listen(5000, async () => {
     console.log(err.message);
   }
 });
-//   "bcrypt": "^5.0.1",
-//   "dotenv": "^16.0.0",
-//   "express": "^4.17.3",
-//   "express-validator": "^6.14.0",
-//   "jsonwebtoken": "^8.5.1",
-//   "mongoose": "^6.2.7"
-// }
